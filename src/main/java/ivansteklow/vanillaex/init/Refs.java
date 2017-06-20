bb22eae32283feb5a8fb7d6fd60da26e1786271a
IvanSteklow <ivansteklow@gmail.com>
2017-06-20 14:51:29 +0300

    Dependency bug fix

--- src/main/java/ivansteklow/vanillaex/init/Refs.java
+++ src/main/java/ivansteklow/vanillaex/init/Refs.java
@@ -17,7 +17,7 @@ public class Refs {
 
 	public static final String MOD_ID = "vanillaex";
 	public static final String NAME = "Vanilla Extras";
-	public static final String VERSION = "1.11.2-1.0.0";
+	public static final String VERSION = "1.0.0";
 	public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 	public static final String MOD_DEPENDENCIES = "required-after:isdev@[2.0.0,);"
 			+ "required-after:forge@[13.20.0.2228,);";
